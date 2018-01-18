5176f169ea345c6b297d3ee6773551d25f3ad155
kimyh5392 <mk.kimyh@gmail.com>
2018-01-18 18:31:05 +0900

    메인 레이아웃 구성 퍼블리싱 작업중 . . . #1

--- index.php
+++ index.php
@@ -17,8 +17,19 @@
     </head>
     <body class="">
         <header id="header">
-            <div class="container">
-                <h1>Header</h1>
+            <div class="top-nav visible-lg">
+                <div class="container">
+                    <ul class="quick-menu">
+                        <li><a href="#">HOME</a></li>
+                        <li><a href="#">CONTACT</a></li>
+                        <li><a href="#">MYPAGE</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="logo-nav">
+                <div class="container">
+                    <h1><a href="/"><img src="/assets/images/logo.jpg" alt=""></a></h1>
+                </div>
             </div>
         </header>
         <main id="content">
@@ -27,8 +38,79 @@
             </div>
         </main>
         <footer id="footer">
+            <div class="donation-information">
+                <div class="container">
+                    <ul class="row">
+                        <li class="col-xs-6 col-sm-3">
+                            <a href="#">
+                                <h3>기부금 내역조회</h3>
+                                <p class="visible-lg">
+                                    당신의 기부금 내역을 조회하실수<br>있습니다.
+                                </p>
+                            </a>
+                        </li>
+                        <li class="col-xs-6 col-sm-3">
+                            <a href="#">
+                                <h3>기금의 종류</h3>
+                                <p class="visible-lg">
+                                    발전기금 기부자의 선택에 따라<br>여러 기금으로 운영됩니다.
+                                </p>
+                            </a>
+                        </li>
+                        <li class="col-xs-6 col-sm-3">
+                            <a href="#">
+                                <h3>기부자 예우</h3>
+                                <p class="visible-lg">
+                                    당신의 큰 사랑에 모든 정성으로<br>보답하겠습니다.
+                                </p>
+                            </a>
+                        </li>
+                        <li class="col-xs-6 col-sm-3">
+                            <a href="#">
+                                <h3>세제혜택</h3>
+                                <p class="visible-lg">
+                                    출연되는 기부금은 세제혜택을<br>받으실 수 있습니다.
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
             <div class="container">
-                <h1>footer</h1>
+                <div class="row">
+                    <div class="col-xs-12 col-lg-5">
+                        <div class="row">
+                            <div class="col-sm-4 col-lg-12">
+                                <h2 class="foot-logo"><img src="/assets/images/foot_logo.png" alt=""></h2>
+                            </div>
+                            <div class="col-sm-8 col-lg-12">
+                                <ul class="policy">
+                                    <li><a href="#">고등과학원소개</a></li>
+                                    <li><a href="#">이용약관</a></li>
+                                    <li><a href="#">개인정보취급방침</a></li>
+                                    <li><a href="#">기부문의</a></li>
+                                </ul>
+                                <div class="company-info">
+                                    <ul>
+                                        <li>서울시 동대문구 회기로 85(207-43) 고등과학원</li>
+                                        <li>TEL : 02-958-3780</li>
+                                        <li>FAX : 02-958-3770</li>
+                                        <li>E-MAIL : <a href="mailto:sckwak@kias.re.kr">sckwak@kias.re.kr</a></li>
+                                        <li class="hidden-lg">입금계좌 (우리은행) : 254-040273-01-001</li>
+                                    </ul>
+                                    <p class="copyright">(C) 2017 KIAS. ALL RIGHT RESERVED.</p>
+                                    <p class="site-by">SITE BY <a href="http://movementk.co.kr/" target="_blank">MOVEMENT K</a> GROUP.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 visible-lg">
+                        입금계좌 안내
+                    </div>
+                    <div class="col-lg-3 visible-lg">
+                        문의전화
+                    </div>
+                </div>
             </div>
         </footer>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
