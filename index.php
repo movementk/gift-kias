68d12030d105546ca739c81456d119ea36051d6f
kimyh5392 <mk.kimyh@gmail.com>
2018-01-18 15:58:31 +0900

    고등과학원 기금 사이트 메인 퍼블리싱 작업중. . .

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>고등과학원 기금사이트</title>
+        <link href="//fonts.googleapis.com/earlyaccess/notosanskr.css" rel="stylesheet">
+        <link href="//fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+        <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/assets/fontello/css/fontello.css" rel="stylesheet">
+        <link href="/assets/css/default.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body class="">
+        <header id="header">
+            <div class="container">
+                <h1>Header</h1>
+            </div>
+        </header>
+        <main id="content">
+            <div class="container">
+                <h1>content</h1>
+            </div>
+        </main>
+        <footer id="footer">
+            <div class="container">
+                <h1>footer</h1>
+            </div>
+        </footer>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
