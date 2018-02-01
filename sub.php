c3151de36a182824a1814ccd8b06ef3fc2ad372d
kimyh5392 <mk.kimyh@gmail.com>
2018-02-01 17:49:47 +0900

    아라 퍼블리싱 연습중

---
+++ sub.php
@@ -0,0 +1,60 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/dochead.php'); ?>
+<link href="/assets/css/sub.css" rel="stylesheet">
+</head>
+<body class="sub "> <!-- 폴더별 클래스 -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/header.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/nav-aisde.php'); ?>
+    <!-- Content Start -->
+    <!-- 페이지 헤더 -->
+    <div class="page-header">
+        <h2 class="page-title">ABOUT KIAS</h2>
+        <ol class="breadcrumb">
+            <li>
+                <a href="/">
+                    <i class="icon-home"><span class="sr-only">메인으로</span></i>
+                </a>
+            </li>
+            <li>기금소개</li>
+            <li>기금용도</li>
+        </ol>
+    </div>
+    <main id="content" class=""><!-- 페이지명 클래스 -->
+        <div class="container-fluid">
+            <div class="page-warp">
+                <div class="row">
+                    <div class="col-xs-12 col-lg-3">
+                        <nav class="lnb-nav">
+                            <h3 class="lnb-title visible-lg">
+                                기금소개
+                                <small>Aobut KIAS</small>
+                            </h3>
+                            <ul>
+                                <li><a href="#">인사말</a></li>
+                                <li class="active"><a href="#">기금용도</a></li>
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">찾아오시는길</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="col-xs-12 col-lg-9">
+                        <div class="sub-content"><!-- sub content -->
+                            <section>
+                                <div class="section-header">
+                                    <h4 class="section-title">기금용도</h4>
+                                </div>
+                                <div class="section-content">
+                                    
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <!-- Content End -->
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/footer.php'); ?>
+    <?php require_once($_SERVER['DOCUMENT_ROOT'].'/inc/docfoot.php'); ?>
+    <script src="/assets/js/common.js"></script>
+</body>
+</html>
