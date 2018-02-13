fba9ec8788e116c37b3ec2097cc0e49c86c68e21
Joahra <mk_ahra0522@gmail.com>
2018-02-13 18:46:20 +0900

    메일 완료.

---
+++ mail.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ko">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Title</title>
+    </head>
+    <body>
+        <div style="width: 700px; margin: 0 auto; padding: 100px 60px; padding: 100px 60px;">
+            <h1 style="text-align:left; margin-bottom: 20px;">
+                <img src="http://html.gift-kias.co.kr/assets/images/logo.jpg" alt="고등과학원 로고">
+            </h1>
+            <div style="border-top: 2px solid #143883;">
+                <table style="width: 100%;">
+                    <tbody style="text-align: left;">
+                        <tr>
+                            <th style="width: 170px; padding: 20px; font-size: 15px; color: #333; border-bottom: 1px solid #dfdfdf; background-color: #f6f6f8;">이름</th>
+                            <td style="padding: 20px 30px; font-size: 14px; color: #666; font-weight: 400; border-bottom: 1px solid #dfdfdf;">홍길동</td>
+                        </tr>
+                        <tr>
+                            <th style="width: 170px; padding: 20px; font-size: 15px; color: #333; border-bottom: 1px solid #dfdfdf; background-color: #f6f6f8;">연락처</th>
+                            <td style="padding: 20px 30px; font-size: 14px; color: #666; font-weight: 400; border-bottom: 1px solid #dfdfdf;">010-0000-0000</td>
+                        </tr>
+                        <tr>
+                            <th style="width: 170px; padding: 20px; font-size: 15px; color: #333; border-bottom: 1px solid #dfdfdf; background-color: #f6f6f8;">이메일</th>
+                            <td style="padding: 20px 30px; font-size: 14px; color: #666; font-weight: 400; border-bottom: 1px solid #dfdfdf;">msh@kias.re.kr</td>
+                        </tr>
+                        <tr>
+                            <th style="width: 170px; padding: 20px; font-size: 15px; color: #333; border-bottom: 1px solid #dfdfdf; background-color: #f6f6f8;">기부금액</th>
+                            <td style="padding: 20px 30px; font-size: 14px; color: #666; font-weight: 400; border-bottom: 1px solid #dfdfdf;">1,000,000원</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
