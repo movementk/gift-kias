f0f06f2bc2a4d882f3102a6b12f9c976f39505be
kimyh5392 <mk.kimyh@gmail.com>
2018-03-05 18:23:06 +0900

    수정완료

--- mail.php
+++ mail.php
@@ -8,8 +8,12 @@
     </head>
     <body>
         <div style="width: 700px; margin: 0 auto; padding: 100px 60px; padding: 100px 60px;">
-            <h1 style="text-align:left; margin-bottom: 20px;">
+            <p style="line-height:28px;margin-bottom: 50px; font-size: 15px; color: #333; font-weight: 400; ">
+                안녕하세요! 홍길동님<br>고등과학원 기금에 관심을 가져주셔서 대단히 감사합니다.<br><br>기부자님께서 등록해주신 약정서의 내용을 보내드립니다.<br><br>후원코드로 기부내역을 조회하실 수 있으시며, 기부하시는데 번거로움을 덜어드리기 위하여 <br>저희 담당자가 등록해주신 연락처로 직접 연락을 드려 후속 진행을 도와드리도록 하겠습니다.
+            </p>
+            <h1 style="text-align:left; margin-bottom: 20px; position: relative;">
                 <img src="http://html.gift-kias.co.kr/assets/images/logo.jpg" alt="고등과학원 로고">
+                <a href="#" style="position: absolute; top: 0; right: 0; font-size: 15px; line-height: 66px; color: #666; text-decoration: none;">기부금 영수증 출력</a>
             </h1>
             <div style="border-top: 2px solid #143883;">
                 <table style="width: 100%;">
